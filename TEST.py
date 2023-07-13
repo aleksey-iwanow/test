5a4f89461b8f7e733f2514580a61df9342580ebc
Лёха <83726458+aleksey-iwanow@users.noreply.github.com>
2023-07-14 00:03:21 +0300

    your_commit_message

--- TEST.py
+++ TEST.py
@@ -1,12 +1,1 @@
-from PIL import Image
-
-
-with Image.open('ии2.jpg') as im:
-    pixels = im.load()
-
-    for i in range(im.size[0]):
-        for j in range(im.size[1]):
-            r, g, b = pixels[i, j]
-            pixels[i, j] = 0, g if 51 < g < 200 else 0, 0
-
-    im.save("hh.png")
+your_new_file_content
